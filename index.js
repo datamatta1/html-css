bbb954ddac6212f73b99561da07e03bd54879aff
buck80 <mkomljenovic11@gmail.com>
2022-05-26 15:01:42 +0200

    add interactivity

---
+++ index.js
@@ -0,0 +1,11 @@
+const menu = document.querySelector(".menu");
+const nav = document.querySelector("nav");
+const close = document.querySelector(".close");
+
+menu.addEventListener("click", () => {
+  nav.classList.add("open-nav");
+});
+
+close.addEventListener("click", () => {
+  nav.classList.remove("open-nav");
+});
